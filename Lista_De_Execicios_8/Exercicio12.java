9eb41ade46b464b21daf8243aa90d16ecb084102
VKTGabriel <victorpedrosa66@yahoo.com.br>
2024-06-20 09:11:41 -0300

    Atualização

--- Lista_De_Execicios_8/Exercicio12.java
+++ Lista_De_Execicios_8/Exercicio12.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Exercicio12 {
     public static void main(String[] args) {
-        int[][] matrix = new int[5][5];
+        int[][] matriz = new int[5][5];
         Scanner ler = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.printf("[linha %d - coluna %d] Digie um número:", i + 1, j + 1);
-                matrix[i][j] = ler.nextInt();
+                matriz[i][j] = ler.nextInt();
             }
         }
 
@@ -18,14 +18,14 @@ public class Exercicio12 {
         System.out.printf("\nMatriz:\n");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                System.out.printf("%d\t", matrix[i][j]);
+                System.out.printf("%d\t", matriz[i][j]);
             }
             System.out.printf("\n");
         }
 
         for (int i = 0; i < 5; i++) {
-            diagonalP += matrix[i][i];
-            diagonalS += matrix[i][4-i];
+            diagonalP += matriz[i][i];
+            diagonalS += matriz[i][4-i];
         }
 
         System.out.printf("\nDiagonal Principal: %d\nDiagonal Secundaria: %d", diagonalP, diagonalS);
